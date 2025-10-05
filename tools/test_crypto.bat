abfb6702726384e05d525ea7e87442ffba8a330b
Georg Doll <georg.m.doll@gmail.com>
2025-10-05 16:16:54 +0000

    refactor(tools): Remove obsolete test scripts for cryptography and certificate generation

--- tools/test_crypto.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Testing cryptography library in ESP-IDF environment...
-cd /D C:\workspace\ESP32_Projects\distance
-call C:\workspace\ESP32_Projects\esp\v5.4.1\esp-idf\export.bat
-python tools\test_crypto.py
-pause
