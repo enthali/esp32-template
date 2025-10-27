70ad180094305f40fb7d8e0d154327521cce4aa0
Georg <georg.m.doll@gmail.com>
2025-10-27 23:04:47 +0000

    feat(devcontainer): Add postAttachCommand and on-attach script for VNC permissions setup

---
+++ tools/on-attach.sh
@@ -0,0 +1,8 @@
+#!bash
+# on-attach.sh
+
+# VNC Startup Script for ESP32 Template
+# This script ensures necessary permissions for VNC operation in the development container
+sudo chmod 1777 /tmp
+
+date > .on-attach-ran
